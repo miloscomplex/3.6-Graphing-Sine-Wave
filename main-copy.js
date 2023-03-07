d81afa6dcc6b3ca10728c3b5aa643c017903ee81
Michael Sutton <hicallmesutton@gmail.com>
2023-03-07 14:30:32 -0600

    made a copy to play around with the script

--- main-copy.js
+++ main-copy.js
@@ -3,11 +3,11 @@ let angleV = [];
 let r = 4;
 
 function setup() {
-  createCanvas(600, 400);
-  background(0);
+  createCanvas(1000, 400);
+  background(0, 20);
   let total = floor(width / (r*2));
   for (let i = 0; i < total; i++) {
-    angles[i] = map(i, 0, total+1, 0, 2 * TWO_PI);
+    angles[i] = map(i, 0, total+1, 0, TWO_PI);
     // TWO_PI is one cycle of sin
     angleV[i] = 0.01 + i / 100;
   }
@@ -24,12 +24,12 @@ function draw() {
   for (let i = 0; i < angles.length; i++) {
     let y = map(sin(angles[i]), -1, 1, -180, 180);
     strokeWeight(4);
-    let x = map(i, 0, angles.length, -280, 300);
+    let x = map(i, 0, angles.length, -480, 480);
     //line(x, 0, x, y);
     circle(x, y, r);
-    // vertex(x, y);
+    //vertex(x, y);
     angles[i] += 0.02;
-    //angleV[i] += 0.0001;
+    angleV[i] += 0.0001;
   }
   endShape();
   //angleV += 0.0001;
